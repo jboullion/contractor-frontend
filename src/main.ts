494bb487276770260c1ff88c5a33a57e0b1fc059
jboullion <jboullion83@gmail.com>
2021-11-11 23:40:21 -0600

    PWA Setup!
    Using the vite-pwa-plugin to setup a basic PWA!

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './index.css'
+import { registerSW } from 'virtual:pwa-register'
 
 createApp(App).mount('#app')
+
+const intervalMS = 60 * 60 * 1000
+
+const updateSW = registerSW({
+  onRegistered(r) {
+    r && setInterval(() => {
+      r.update()
+    }, intervalMS)
+  },
+  onNeedRefresh() {},
+  onOfflineReady() { },
+  //onRegisterError(error) {}
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,38 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],  
+      manifest: {
+        name: 'Contractor Checklist',
+        short_name: 'DIY Checklist',
+        description: 'Track your jobs, materials, employees, and clients easily!',
+        theme_color: '#059669',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          }
+        ]
+      }
+    })
+  ]
 })
