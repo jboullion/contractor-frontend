24bb9213b095d03ac3b0e449f1d013f238c8c3d4
jboullion <jboullion83@gmail.com>
2021-11-13 23:06:55 -0600

    Reconfigured routes. Setup marketing / landing page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,24 @@
 import { createWebHistory, createRouter } from 'vue-router';
 
 // Lazy Load pages
+const Home = () => import('../views/layouts/Home.vue');
+const Auth = () => import('../views/layouts/Authentication.vue');
+const Application = () => import('../views/layouts/Application.vue');
+
 const Login = () => import('../views/auth/Login.vue');
+const Register = () => import('../views/auth/Register.vue');
 
-const Dashboard = () => import('../views/Dashboard.vue');
+const Dashboard = () => import('../views/app/Dashboard.vue');
 
-const JobsList = () => import('../views/jobs/JobsList.vue');
-const JobsCreate = () => import('../views/jobs/JobsCreate.vue');
-const JobsView = () => import('../views/jobs/JobsView.vue');
-const JobsEdit = () => import('../views/jobs/JobsEdit.vue');
+const JobsList = () => import('../views/app/jobs/JobsList.vue');
+const JobsCreate = () => import('../views/app/jobs/JobsCreate.vue');
+const JobsView = () => import('../views/app/jobs/JobsView.vue');
+const JobsEdit = () => import('../views/app/jobs/JobsEdit.vue');
 
 // const Experiments = () => import("../components/pages/Experiments.vue");
 // const Futurist = () => import("../components/pages/Futurist.vue");
 // const Games = () => import("../components/pages/Games.vue");
-const NotFound = () => import('../views/NotFound.vue');
+const NotFound = () => import('../views/layouts/NotFound.vue');
 
 const requiresAuth = {
   requiresAuth: true,
@@ -26,40 +31,53 @@ const redirectAuth = {
 const routes = [
   {
     path: '/',
-    name: 'Dashboard',
-    component: Dashboard, // TODO: This should be the marketing / landing page eventually
+    name: 'Home',
+    component: Home,
   },
   {
-    path: '/login',
+    path: '/auth',
     component: Login,
     meta: redirectAuth,
+    children: [
+      {
+        path: '/login',
+        component: Login,
+      },
+      {
+        path: '/register',
+        component: Register,
+      },
+    ],
   },
   {
-    path: '/dashboard',
-    name: 'Dashboard',
-    component: Dashboard,
+    path: '/app',
+    name: 'Application',
+    component: Application,
     meta: requiresAuth,
-  },
-  {
-    path: '/jobs',
-    name: 'Jobs',
-    component: JobsList,
-    meta: requiresAuth,
-  },
-  {
-    path: '/jobs/create',
-    component: JobsCreate,
-    meta: requiresAuth,
-  },
-  {
-    path: '/jobs/:id',
-    component: JobsView,
-    meta: requiresAuth,
-  },
-  {
-    path: '/jobs/:id/edit',
-    component: JobsEdit,
-    meta: requiresAuth,
+    children: [
+      {
+        path: '/dashboard',
+        name: 'Dashboard',
+        component: Dashboard,
+      },
+      {
+        path: '/jobs',
+        name: 'Jobs',
+        component: JobsList,
+      },
+      {
+        path: '/jobs/create',
+        component: JobsCreate,
+      },
+      {
+        path: '/jobs/:id',
+        component: JobsView,
+      },
+      {
+        path: '/jobs/:id/edit',
+        component: JobsEdit,
+      },
+    ],
   },
   {
     path: '/:catchAll(.*)',
---
+++ src/views/layouts/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Hero />
+    <Pricing />
+    <Footer />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Hero from '../marketing/Hero.vue';
+import Pricing from '../marketing/Pricing.vue';
+import Footer from '../marketing/Footer.vue';
+</script>
+
+<style></style>
