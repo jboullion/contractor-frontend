1165f562721df860624fd66b58eb645b28c9b326
jboullion <jboullion83@gmail.com>
2021-11-13 23:18:22 -0600

    404 Page Setup

--- src/views/layouts/NotFound.vue
+++ src/views/layouts/NotFound.vue
@@ -1,105 +1,78 @@
-<!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-  <!--
-    This example requires updating your template:
-
-    ```
-    <html class="h-full">
-    <body class="h-full">
-    ```
-  -->
-  <div class="min-h-full pt-16 pb-12 flex flex-col bg-white">
-    <main
-      class="
-        flex-grow flex flex-col
-        justify-center
-        max-w-7xl
-        w-full
-        mx-auto
-        px-4
-        sm:px-6
-        lg:px-8
-      "
-    >
-      <div class="flex-shrink-0 flex justify-center">
-        <a href="/" class="inline-flex">
-          <span class="sr-only">Workflow</span>
-          <img
-            class="h-12 w-auto"
-            src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600"
-            alt=""
-          />
-        </a>
-      </div>
-      <div class="py-16">
-        <div class="text-center">
-          <p
-            class="
-              text-sm
-              font-semibold
-              text-indigo-600
-              uppercase
-              tracking-wide
-            "
-          >
-            404 error
-          </p>
-          <h1
-            class="
-              mt-2
-              text-4xl
-              font-extrabold
-              text-gray-900
-              tracking-tight
-              sm:text-5xl
-            "
-          >
-            Page not found.
-          </h1>
-          <p class="mt-2 text-base text-gray-500">
-            Sorry, we couldn’t find the page you’re looking for.
-          </p>
-          <div class="mt-6">
-            <a
-              href="#"
+  <div class="relative pt-6 bg-white min-h-full flex flex-col">
+    <Header />
+    <div class="pt-16 pb-12 flex-grow">
+      <main
+        class="
+          flex flex-col
+          justify-center
+          max-w-7xl
+          w-full
+          mx-auto
+          px-4
+          sm:px-6
+          lg:px-8
+        "
+      >
+        <div class="flex-shrink-0 flex justify-center">
+          <a href="/" class="inline-flex">
+            <span class="sr-only">Workflow</span>
+            <img
+              class="h-12 w-auto"
+              src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600"
+              alt=""
+            />
+          </a>
+        </div>
+        <div class="py-16">
+          <div class="text-center">
+            <p
               class="
-                text-base
-                font-medium
+                text-sm
+                font-semibold
                 text-indigo-600
-                hover:text-indigo-500
+                uppercase
+                tracking-wide
               "
-              >Go back home<span aria-hidden="true"> &rarr;</span></a
             >
+              404 error
+            </p>
+            <h1
+              class="
+                mt-2
+                text-4xl
+                font-extrabold
+                text-gray-900
+                tracking-tight
+                sm:text-5xl
+              "
+            >
+              Page not found.
+            </h1>
+            <p class="mt-2 text-base text-gray-500">
+              Sorry, we couldn’t find the page you’re looking for.
+            </p>
+            <div class="mt-6">
+              <a
+                href="#"
+                class="
+                  text-base
+                  font-medium
+                  text-indigo-600
+                  hover:text-indigo-500
+                "
+                >Go back home<span aria-hidden="true"> &rarr;</span></a
+              >
+            </div>
           </div>
         </div>
-      </div>
-    </main>
-    <footer class="flex-shrink-0 max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
-      <nav class="flex justify-center space-x-4">
-        <a
-          href="#"
-          class="text-sm font-medium text-gray-500 hover:text-gray-600"
-          >Contact Support</a
-        >
-        <span
-          class="inline-block border-l border-gray-300"
-          aria-hidden="true"
-        />
-        <a
-          href="#"
-          class="text-sm font-medium text-gray-500 hover:text-gray-600"
-          >Status</a
-        >
-        <span
-          class="inline-block border-l border-gray-300"
-          aria-hidden="true"
-        />
-        <a
-          href="#"
-          class="text-sm font-medium text-gray-500 hover:text-gray-600"
-          >Twitter</a
-        >
-      </nav>
-    </footer>
+      </main>
+    </div>
+    <Footer />
   </div>
 </template>
+
+<script setup lang="ts">
+import Header from '../marketing/Header.vue';
+import Footer from '../marketing/Footer.vue';
+</script>
